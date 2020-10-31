da007f2d7eadff59de9b62a7bfbd5d8b3e0640a4
Karolina Kulinska <karolina.kulinska89@gmail.com>
2020-10-31 13:07:49 +0100

    add responsive style for mobile vesrion to each section xs=375px

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
-//  Co trzeba zrobić kroki 
+//  Co trzeba zrobić kroki
 // napisać funkcjonaloność, która po kliknięciu w pytanie czyli nasze ramkę z pytaniem (accordion__item),
 // otrzyma ona klase (.show-accordion) która pokazuje odpowiedz na pytanie oraz
-// w tym samym kliknięciu musimy zamienić strzałki (.arrow-remove) musi dostac display:block , a arrow-add display none . 
+// w tym samym kliknięciu musimy zamienić strzałki (.arrow-remove) musi dostac display:block , a arrow-add display none .
 
+//Próba z wszystkimi pytaniami
 
-//Próba z wszystkimi pytaniami 
+// const questions = document.querySelectorAll('.accordion__item')
+//  zwraca nam kolekcje elementów
+const arrowRemove = document.querySelector(".arrow-remove");
+const arrowAdd = document.querySelector(".arrow-add");
+const answer = document.querySelector(".accordion__answer");
 
-const questions = document.querySelectorAll('.accordion__item')
-//  zwraca nam kolekcje elementów 
-const arrowRemove = document.querySelector('.arrow-remove')
-const arrowAdd = document.querySelector('.arrow-add')
-const answer = document.querySelector('.accordion__answer')
-
-
-// PRÓBA Z WSZYTKIMI PYTANIAMI 
+// PRÓBA Z WSZYTKIMI PYTANIAMI
 
 // questions.forEach(item => {
 //             item.addEventListener('click', openAccordion);
-
 
 //             function openAccordion(event) {
 //                 let isClicking = item.contains(event.target)
@@ -30,45 +27,45 @@ const answer = document.querySelector('.accordion__answer')
 //             }
 //      })
 
+//  próba z jednym pytaniem
 
-//  próba z jednym pytaniem 
+// const question1 = document.getElementById('question1');
 
-const question1 = document.getElementById('question1');
+// question1.addEventListener('click', openAccordion)
 
+// function openAccordion(event) {
+//     let isClicking = question1.contains(event.target)
+//     if (isClicking) {
+//         answer.classList.toggle('show-accordion')
+//     }
 
-question1.addEventListener('click', openAccordion)
+//     if (answer.classList.contains('show-accordion')) {
+//         arrowRemove.style.display = "block"
+//         arrowAdd.style.display = "none"
+//     } else {
+//         arrowRemove.style.display = "none"
+//         arrowAdd.style.display = "block"
+//     }
 
-function openAccordion(event) {
-    let isClicking = question1.contains(event.target)
-    if (isClicking) {
-        answer.classList.toggle('show-accordion')
-    }
+// }
 
-    if (answer.classList.contains('show-accordion')) {
-        arrowRemove.style.display = "block"
-        arrowAdd.style.display = "none"
-    } else {
-        arrowRemove.style.display = "none"
-        arrowAdd.style.display = "block"
-    }
+const questions = document.querySelectorAll(".accordion__item");
 
+questions.forEach((item) => {
+  const answer = item.querySelector(".accordion__answer");
+  item.addEventListener("click", () => {
+    answer.classList.toggle("show-accordion");
+  });
+});
 
+// Hambuger menu
 
-}
+const menuBtn = document.querySelector(".menu-btn");
+const navBarLinks = document.querySelector(".navbar-links");
 
-
-
-
-// Hambuger menu 
-
-const menuBtn = document.querySelector('.menu-btn')
-const navBarLinks = document.querySelector('.navbar-links')
-
-menuBtn.addEventListener('click', toggleMenu)
+menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-menuBtn.classList.toggle('close')
-navBarLinks.classList.toggle('active')
-
-
+  menuBtn.classList.toggle("close");
+  navBarLinks.classList.toggle("active");
 }
