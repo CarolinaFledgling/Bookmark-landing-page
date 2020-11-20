1e6602825d46838d276ba16e2337939dd6c48ba4
Karolina Kulinska <karolina.kulinska89@gmail.com>
2020-11-20 20:44:19 +0100

    fixed arrows red and blue when users clicking outside of questions

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ questions.forEach((item) => {
     if (answer.classList.contains('show-accordion')) {
       arrowRemove.style.display = "block"
       arrowAdd.style.display = "none"
+      console.log(arrowAdd)
+      console.log(arrowRemove)
     } else {
       arrowRemove.style.display = "none"
       arrowAdd.style.display = "block"
@@ -30,13 +32,17 @@ document.addEventListener('click', (event) => {
     let isClickInsideQuestion = question.contains(event.target);
     //sprawdza to miejsce które jest kliknięte 
 
-    console.log(isClickInsideQuestion, question, event.target)
+    // console.log(isClickInsideQuestion, question, event.target)
 
     if (isClickInsideQuestion === false) {
       
       const answer = question.querySelector('.accordion__answer')
-      console.log("Answer", question, answer)
+      // console.log("Answer", question, answer)
       answer.classList.remove("show-accordion")
+      const arrowRemove = question.querySelector('.arrow-remove')
+      const arrowAdd = question.querySelector('.arrow-add')
+      arrowRemove.style.display = "none"
+      arrowAdd.style.display = "block"
       
     }
   })
